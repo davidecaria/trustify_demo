0ba0e8ba28c0cfd533eb14d6f5b83e249a9c00f0
s317438 <s317438@studenti.polito.it>
2023-05-27 19:11:21 +0200

    authentication api implemented

--- authentication_server/Model.js
+++ authentication_server/Model.js
@@ -3,20 +3,56 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {
+
+    walletPublicKey: {
         type: String,
         required: true,
-        max: 255,
-        unique: true
+        unique: true,
     },
+
+    //passkeys are stored inside wallet array
+    wallet: [{
+
+        relyingPartyId: {
+            type: String,
+            required: true,
+            max: 255,
+            unique: true
+        },
+
+        relyingPartyName: {
+            type: String,
+            required: true,
+            max: 255,
+        },
+
+        username: {
+            type: String,
+            required: true,
+            max: 255,
+            unique: true
+        },
+
+        passkeyPublicKey: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        
+        //signature of passkey made with the wallet private key
+        passkeySignature: {
+            type: String,
+            required: true,
+            unique: true
+        }
+    }]
 });
 
-// Define a schema for the nonce
+// Define a schema for nonce secure handling during authentication
 const usedNonceSchema = new mongoose.Schema({
-    username: {
+    walletPublicKey: {
         type: String,
         required: true,
-        unique: true
     },
 
     nonce: {
@@ -29,14 +65,10 @@ const usedNonceSchema = new mongoose.Schema({
         type: Date,
         default: Date.now(),
     },
-
-    used: {
-        type: Boolean,
-        default: false,
-    }
 });
 
 // Create a model based on the schema
 const UsedNonce = mongoose.model('UsedNonce', usedNonceSchema);
+const UserSchema = mongoose.model('UserSchema', userSchema);
 
-module.exports = { UsedNonce };
+module.exports = { UsedNonce, UserSchema };
--- authentication_server/controller.js
+++ authentication_server/controller.js
@@ -1,24 +1,24 @@
 "use strict";
 const crypto = require("crypto");
-const { UsedNonce } = require("./Model");
+const moment = require('moment');
+const { UsedNonce, UserSchema } = require("./Model");
 
 
 
 /** - This route is called by the client application to initiate the authentication process:
  *  - Request Body: None 
  *  - Response Body: the challenge (Random Nonce) to solve in order to authenticate
- *    @param username
- *    in order to temporarily store the output nonce in order to later correctly 
+ *    @param walletPublicKey
+ *    to temporarily store the output nonce in order to later correctly 
  *    perform authentication validation
 */
 const generateChallenge = async (request, response) => {
-
     try {
-        if (!request.query.username || request.query.username.trim() === "") {
-            return response.status(400).json({ error: "Username is missing" });
+        if (!request.query.walletPublicKey || request.query.walletPublicKey.trim() === "") {
+            return response.status(400).json({ error: "walletPublicKey is missing" });
         }
 
-        const username = request.query.username.trim();
+        const walletPublicKey = request.query.walletPublicKey.trim();
         const nonce = crypto.randomBytes(16).toString("hex");
 
         const hash = crypto.createHash("sha256");
@@ -28,39 +28,62 @@ const generateChallenge = async (request, response) => {
         const challenge = hash.digest("hex");
 
         // store nonces to keep track of the one been sent and avoid replay attacks
-        await UsedNonce.create({ username, nonce });
+        await UsedNonce.create({ walletPublicKey: walletPublicKey, nonce: nonce });
 
         return response.status(200).json({ challenge: challenge });
     } catch (error) {
         return response.status(400).json({ error: error });
     }
-
 };
 
 
 /** - This route is called by the client application to answer to an asymmetric challenge-response
- *  - Request Body: contains the signed nonce (previously received by the server), claimant's username, plaintext nonce
+ *  - Request Body: contains the signed nonce previously received by the server (response), claimant's wallet's public key, plaintext nonce
  *  - Response Body: contains a message expressing if authentication is successful or not
 */
 const authenticate = async (request, response) => {
-
     try {
-        if (!request.body.hasOwnProperty("username") || !request.body.hasOwnProperty("challenge")) {
+        if (!request.body.hasOwnProperty("walletPublicKey") || !request.body.hasOwnProperty("response") || !request.body.hasOwnProperty("nonce")) {
             return response.status(400).json({ error: "Parameters are missing" });
         }
 
-        if (!request.body.username || !request.body.challenge) {
+        if (!request.body.walletPublicKey || !request.body.response || !request.body.nonce) {
             return response.status(400).json({ error: "Empty Parameters" });
         }
 
-        const { username, challenge } = req.body;
+        const { walletPublicKey, response, nonce } = req.body;
 
-         
+        const authenticationMaterial = await UsedNonce.find({ walletPublicKey: walletPublicKey, nonce: nonce });
 
+        //nonce was not issued by server or "belongs" to a different user
+        if (!authenticationMaterial) {
+            return response.status(401).json({ flag: false, error: "Unauthorized" });
+        }
 
+        //check if nonce has not expired
+        const currentDate = moment();
+        const duration = moment.duration(currentDate.diff(authenticationMaterial.validity));
+        const minutesDiff = duration.asMinutes();
 
+        if (minutesDiff > 1) {
+            return response.status(400).json({ flag: false, error: "Nonce expired" });
+        }
+
+        const passkey = await UserSchema.find({ walletPublicKey: walletPublicKey });
+
+        //check signature
+        const verifier = crypto.createVerify('RSA-SHA256');
+        verifier.update(nonce);
+
+        const isSignatureValid = verifier.verify(passkey.wallet.passkeyPublicKey, response, 'base64');
+
+        if (!isSignatureValid) {
+            return response.status(400).json({ flag: false, error: "Authentication failed" });
+        }
+
+        return response.status(400).json({ flag: true, message: "Authentication succeeded" });
     } catch (error) {
-        return response.status(400).json({ error: error });
+        return response.status(400).json({ flag: false, error: error });
     }
 };
 
